2728c8c6239fe221196c9ab45e3c36dfcf1287e0
Ryan H <ryan@ryanjh.com>
2026-05-21 19:04:13 -0700

    feat: add suu alias

--- roles/create_admin_users/files/ryan.bash
+++ roles/create_admin_users/files/ryan.bash
@@ -1,4 +1,4 @@
 # Bash file containing various aliases I like using.
 
 alias ll="ls -lhFA"
-alias suu='sudo apt update && sudo apt upgrade && sudo apt autoremove'
+alias suu='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && if [ -f /var/run/reboot-required ]; then echo "Reboot required by:"; cat /var/run/reboot-required.pkgs; else echo "Reboot not required."; fi;'
